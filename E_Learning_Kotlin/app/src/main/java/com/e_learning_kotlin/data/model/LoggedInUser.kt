ee719b7e3a8baf5d629df8ac9dba7431ddb851ca
mengchuize <17301075@bjtu.edu.cn>
2019-12-17 10:13:35 +0800

    all

---
+++ E_Learning_Kotlin/app/src/main/java/com/e_learning_kotlin/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package com.e_learning_kotlin.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)

faa2a8dd514a30ca5e62dfb8925b74e3e9d27a0c
mengchuize <17301075@bjtu.edu.cn>
2019-12-17 09:37:37 +0800

    add

---
+++ app/src/main/java/com/e_learning_kotlin/My/classActivity.kt
@@ -0,0 +1,13 @@
+package com.e_learning_kotlin.My
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.e_learning_kotlin.R
+
+class classActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_class)
+    }
+}

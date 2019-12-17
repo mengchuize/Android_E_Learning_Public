61711fc275262e92ee9f35fc0c2df97802467b38
mengchuize <17301075@bjtu.edu.cn>
2019-12-17 08:33:25 +0800

    del

--- E_Learning_Kotlin/app/src/main/java/com/e_learning_kotlin/ui/home/course.kt
+++
@@ -1,9 +0,0 @@
-package com.e_learning_kotlin.ui.home
-
-class course(
-    val coursename: String,
-    val courseschool: String,
-    val courseimageid: Int,
-    val schoolimageid: Int,
-    val freeimageid: Int
-)
